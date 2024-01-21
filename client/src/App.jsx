d1b7296a8fe6c151aabb17bc70b9cbc3d272fd2f
king <kingbraynare@gmail.com>
2024-01-22 03:07:00 +0800

    set up the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { About, Home, Dashbord, SignIn, SignUp, Projects } from './pages';
 
 function App() {
   return (
-    <div className="text-3xl">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/sign-in' element={<SignIn />}/>
+        <Route path='/sign-up' element={<SignUp />}/>
+        <Route path='/projects' element={<Projects />}/>
+        <Route path='/dashbord' element={<Dashbord />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
