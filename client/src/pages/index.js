32fe558dc4ee9bbb9af9e7defa4038c0b28b0bd8
king <kingbraynare@gmail.com>
2024-01-22 10:15:03 +0800

    format structure of pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,15 @@
+import About from './About';
+import Dashbord from './Dashbord';
+import Home from './Home';
+import Projects from './Projects';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+export {
+    About,
+    Dashbord,
+    Home,
+    Projects,
+    SignIn,
+    SignUp,
+}
