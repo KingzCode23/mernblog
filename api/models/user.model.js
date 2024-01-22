4526ca69bc8202ee2ebc29b0b5c1a52322a9960c
king <kingbraynare@gmail.com>
2024-01-22 23:02:07 +0800

    setup model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,9 +1,26 @@
 import mongoose from "mongoose";
 
+// user Schema
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
         unique: true,
-    }
-})
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+  }, {timestamps: true}
+);
+
+// user Models
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
